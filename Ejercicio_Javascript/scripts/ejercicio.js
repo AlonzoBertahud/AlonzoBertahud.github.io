d9febe1a4244f213cee78e82e8b7f9a7c8aaefdb
AlonzoBertahud <159202159+AlonzoBertahud@users.noreply.github.com>
2024-03-08 18:48:16 -0700

    Add files via upload

---
+++ Ejercicio_Javascript/scripts/ejercicio.js
@@ -0,0 +1,11 @@
+
+function cambiarColor(){
+    variable_album=document.getElementById("album");
+    console.log(variable_album);
+    variable_album.style.backgroundColor="black";
+}
+function cambiarTamano(){
+    variable_clase=document.getElementsByClassName("imagen")[0]; 
+    variable_clase.style.height="200px";
+    variable_clase.style.width="auto"; 
+}
